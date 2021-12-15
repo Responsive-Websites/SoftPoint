497cb6c63b5db1f168592a5897826c7e0a0d388e
Danila-hub <kontaigabox@mail.ru>
2021-12-15 13:52:53 +0300

    Added pricing block and styles

--- #src/js/script.js
+++ #src/js/script.js
@@ -6,14 +6,16 @@ function testWebP(callback) {
     callback(webP.height == 2);
   };
   webP.src =
-    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
   if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    console.log('work');
+    document.querySelector("body").classList.add("webp");
+    console.log("work");
   } else {
-    document.querySelector('body').classList.add('no-webp');
+    document.querySelector("body").classList.add("no-webp");
   }
 });
+
+// range slider
